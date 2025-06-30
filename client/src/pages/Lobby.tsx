43896c1ebb6e1a5d3099441511e30bf2169f816c
tarakichi <tarakichi48@gmail.com>
2025-06-30 23:54:31 +0900

    lobby restyling

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -8,18 +8,20 @@ export default function Lobby() {
     const [name, setName] = useState("");
 
     const joinGame = () => {
-        if (!socket) return;
-        if (name.trim()) {
+        if (socket && name.trim()) {
             socket.emit("setName", name);
+            // localStorage.setItem("myName", name);
             navigate("/game");
         }
     };
 
     return (
-        <div>
-            <h1>ロビーへようこそ</h1>
-            <input value={name} onChange={e => setName(e.target.value)} placeholder="名前を入力"/>
-            <button onClick={joinGame}>ゲームに参加</button>
+        <div className='min-h-screen bg-gray-900 overflow-hidden flex flex-col items-center justify-center px-5 py-5'>
+            <div>
+                <h1>ロビーへようこそ</h1>
+                <input value={name} onChange={e => setName(e.target.value)} placeholder="名前を入力"/>
+                <button onClick={joinGame}>ゲームに参加</button>
+            </div>
         </div>
     );
 }
