8eab61b6e52f17cf0c66777fe9cc34c96aa054cc
tarakichi <tarakichi48@gmail.com>
2025-07-12 08:53:21 +0900

    change room design

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -66,18 +66,26 @@ export default function Room() {
     const isHost = myId === hostId;
 
     return (
-        <div className="w-screen h-screen bg-zinc-50 p-4">
+        <div className="w-screen h-screen flex flex-col items-center bg-zinc-50 p-4">
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 1 }}
+                className="w-full h-full flex flex-col gap-4 justify-center items-center"
             >
-                <h2 className="text-2xl font-bold mb-4">ルーム: {roomId}</h2>
-                <p className="mb-2">あなたの名前: {nickname}</p>
-                <h3 className="text-xl mb-2">参加ユーザー:</h3>
-                <ul className="list-disc pl-6">
+                <h2 className="font-notosans text-2xl font-bold text-zinc-600">ルーム: {roomId}</h2>
+                <h3 className="font-notosans font-bold text-md text-zinc-400">参加ユーザー</h3>
+                <ul className="w-4/5 flex justify-center items-center overflow-x-auto p-4 border-y border-zinc-200 gap-6">
                     {users.map((user) => (
-                        <li key={user.id}>{user.nickname} {user.id === hostId && "(ホスト)"}</li>
+                        <li key={user.id} className="flex flex-col justify-center items-center gap-2 select-none p-2 shadow-md rounded-md bg-zinc-100">
+                            <div className="w-20 h-20 shadow rounded-md bg-white">
+                                <img src="/sprites/845.png" alt="" className="pointer-events-none"/>
+                            </div>
+                            <div className="w-30 h-7 flex items-center font-notosans text-sm justify-center text-zinc-800 font-semibold shadow rounded-md bg-white overflow-x-auto overflow-y-hidden">
+                                {user.nickname}
+                                {user.id === hostId && <img src="/poke-ball.png" title="ホスト" alt="ホスト" className="inline pointer-events-none"></img>}
+                            </div>
+                        </li>
                     ))}
                 </ul>
 
